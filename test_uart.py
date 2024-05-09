a9a4c3d942575b2e0208866dd6da0fea5c46b985
MinhPhan-CoreVision <minhphan@corevision.vn>
2024-05-09 14:17:47 +0700

    adjust pin name ttyACM0 -> ttyAMA0

--- test_uart.py
+++ test_uart.py
@@ -4,7 +4,7 @@ import time
 print ("hello world!")
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
     ser.reset_input_buffer()
 
     while True:
