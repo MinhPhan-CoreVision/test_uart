00022b4b326082bee9b5c55045a5dc29c63b2680
MinhPhan-CoreVision <minhphan@corevision.vn>
2024-05-09 15:49:19 +0700

    try different init

--- test_uart.py
+++ test_uart.py
@@ -2,11 +2,24 @@ import serial
 import time
 
 print ("hello world!")
+ 
+ser = serial.Serial(
+	port = '/dev/ttyAMA0',
+	baudrate = 9600,
+	parity = serial.PARITY_NONE,
+	stopbits = serial.STOPBITS_ONE,
+	bytesize = serial.EIGHTBITS,
+	timeout = 1
+)
+ser.reset_input_buffer()
 
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
-    ser.reset_input_buffer()
-
+print("Raspberry's sending : ")
+ 
+try:
     while True:
-        ser.write (b"DietPi hello.\n")
-        time.sleep(1)
+    	ser.write(b'hehe')
+    	ser.flush()
+    	print("hehe")
+    	time.sleep(1)
+except KeyboardInterrupt:
+	ser.close()
