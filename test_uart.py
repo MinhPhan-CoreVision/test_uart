7100983fff366452bedd6b14aa1d3328e0fb3044
MinhPhan-CoreVision <minhphan@corevision.vn>
2024-05-09 13:42:53 +0700

    serial to say hello

--- test_uart.py
+++ test_uart.py
@@ -1,1 +1,12 @@
+import serial
+import time
+
 print ("hello world!")
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.reset_input_buffer()
+
+    while True:
+        ser.write (b"DietPi hello.\n")
+        time.sleep(1)
